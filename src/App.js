071c7eb1ce9fd6be10a1367cb863dcd7c9cf3603
Joshua Loehr <loehrj@students.wwu.edu>
2019-04-20 17:15:37 -0700

    Use geolocation to determine intial map location, and add PageLoadMask

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Map from './components/Map';
 
-const getCurrentLocation = () => ([
-    48.7451777,
-    -122.47007679999999
-]);
+const PageLoadMask = ({ loading }) => (
+    <div style={{ width: '100%', height: '98vh', display: loading ? 'flex' : 'none' }}>
+        <img src='loader.gif' alt='loading...' width={200} height={200} style={{ margin: 'auto' }} />
+    </div>
+)
 
-export default () => (
-    <Map position={getCurrentLocation()} />
-);
+export default () => {
+    const [loading, setLoading] = useState(true);
+    const [position, setPosition] = useState(null);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+            setPosition([latitude, longitude]);
+            setLoading(false);
+        }, console.error, {
+            useHighAccuracy: true,
+            timeout: 10000,
+            maximumAge: 0
+        });
+    });
+
+    return (
+        <div>
+            <PageLoadMask loading={loading} />
+            <Map position={position} style={{ display: loading ? 'none' : 'block' }} />
+        </div>
+    );
+};
 
