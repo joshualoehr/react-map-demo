cfe24c50270601a86bf7fbf2b6c727bfec787477
Joshua Loehr <loehrj@students.wwu.edu>
2019-04-23 21:24:15 -0700

    Add polyline drawing

--- src/components/DrawControl.js
+++ src/components/DrawControl.js
@@ -2,28 +2,46 @@ import L from 'leaflet';
 import { withLeaflet, MapControl } from 'react-leaflet';
 
 L.Control.Draw = L.Control.extend({
-    onAdd: map => {
-        const div = L.DomUtil.create('div');
-        div.classList.add('draw-control');
-        div.drawing = false;
-
-        const img = L.DomUtil.create('img');
-        img.srcs = ['pencil-edit-button.svg', 'cancel.svg'];
-        img.src = img.srcs[0];
-        
-        div.append(img);
-        L.DomEvent.on(div, 'click', () => {
-            div.drawing = !div.drawing;
-            img.src = img.srcs[div.drawing ? 1 : 0];
-        });
-
-        return div;
+    getDiv() {
+        if (!this.div) {
+            this.div = L.DomUtil.create('div');
+            this.div.classList.add('draw-control');
+            this.div.drawing = false;
+            this.div.append(this.getImg());
+        }
+        return this.div;
+    },
+    getImg() {
+        if (!this.img) {
+            this.img = L.DomUtil.create('img');
+            this.img.srcs = ['pencil-edit-button.svg', 'cancel.svg'];
+            this.img.src = this.img.srcs[0];
+        }
+        return this.img;
+    },
+    onAdd(map) {
+        return this.getDiv();
     }
 });
 
 class DrawControl extends MapControl {
+
+    constructor(props) {
+        super(props);
+
+        const div = this.leafletElement.getDiv();
+        const img = this.leafletElement.getImg();
+        L.DomEvent.on(div, 'click', () => this.toggleDrawing(div, img));
+    }
+
     createLeafletElement(props) {
         return new L.Control.Draw(props);
+    }
+
+    toggleDrawing = (div, img) => {
+        div.drawing = !div.drawing;
+        img.src = img.srcs[div.drawing ? 1 : 0];
+        this.props.onClick(div.drawing);
     }
 }
 
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Map as LeafletMap, Marker, TileLayer } from 'react-leaflet';
+import { Map as LeafletMap, Marker, Polyline, TileLayer } from 'react-leaflet';
 import CurrentLocationMarker from './CurrentLocationMarker';
 import DrawControl from './DrawControl';
 import { randNorm } from '../util';
@@ -7,19 +7,74 @@ import { randNorm } from '../util';
 const NUM_MARKERS = 20;
 const OFFSETS = Array.from(Array(NUM_MARKERS), () => randNorm(0, 0.005));
 
-export default ({ position }) => {
-    let markerPositions = !position ? [] : OFFSETS.map(offset => position.map((pos, i) => pos + offset[i]));
-    return (
-        <LeafletMap center={position} zoom={15}>
-            <TileLayer
-                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <DrawControl position="topright" />
-            <CurrentLocationMarker position={position} />
-            {markerPositions.map(([lat, lng], idx) => (
-                <Marker key={`marker${idx}`} position={[lat, lng]} />
-            ))}
-        </LeafletMap>
-    );
-};
+export default class Map extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            drawingEnabled: false,
+            drawingActive: false,
+            polylinePositions: []
+        }
+    }
+
+    setDrawingEnabled = (drawingEnabled) => {
+        console.log('setDrawingEnabled', drawingEnabled);
+        this.setState({ drawingEnabled });
+    }
+
+    setDrawingActive = (drawingActive) => {
+        console.log('setDrawingActive', drawingActive);
+        if (drawingActive)
+            this.clear();
+        this.setState({ drawingActive });
+    }
+
+    draw = (position) => {
+        console.log(position);
+        this.setState((state) => ({ polylinePositions: state.polylinePositions.concat(position) }));
+    }
+
+    clear = () => {
+        this.setState({ polylinePositions: [] });
+    }
+
+    render() {
+        const { position } = this.props;
+        const { drawingActive, drawingEnabled, polylinePositions } = this.state;
+        let markerPositions = !position ? [] : OFFSETS.map(offset => position.map((pos, i) => pos + offset[i]));
+        return (
+            <LeafletMap 
+                center={position} 
+                zoom={15} 
+                dragging={!drawingEnabled} 
+                onMousedown={(drawingEnabled && !drawingActive) 
+                    ? () => this.setDrawingActive(true)
+                    : null
+                }
+                onMousemove={(drawingEnabled && drawingActive)
+                    ? ({ latlng }) => this.draw(latlng)
+                    : null
+                }
+                onMouseup={(drawingEnabled && drawingActive)
+                    ? () => this.setDrawingActive(false)
+                    : null
+                }
+            >
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <DrawControl 
+                    position="topright" 
+                    
+                    
+                    onClick={this.setDrawingEnabled}/>
+                <CurrentLocationMarker position={position} />
+                {markerPositions.map(([lat, lng], idx) => (
+                    <Marker key={`marker${idx}`} position={[lat, lng]} />
+                ))}
+                <Polyline color="blue" positions={polylinePositions} />
+            </LeafletMap>
+        );
+    }
+}
