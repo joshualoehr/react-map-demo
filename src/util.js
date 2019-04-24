690f8684c35a3591cb9e7f3c5777e5f06aabe1be
Joshua Loehr <loehrj@students.wwu.edu>
2019-04-23 22:46:38 -0700

    Add freehand drawing selection functionality

--- src/util.js
+++ src/util.js
@@ -7,3 +7,21 @@ export const randNorm = (mu = 0, sigma = 1) => {
 };
 
 export const rotate = (arr, n = 1) => arr.slice(n).concat(arr.slice(0, n));
+
+// adapted from https://github.com/substack/point-in-polygon/blob/master/index.js
+export const polygonContainsPoint = (point, vs) => {
+    vs = vs.map(({lat, lng}) => [lat, lng]);
+    const x = point[0], y = point[1];
+    
+    let inside = false;
+    for (let i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        const xi = vs[i][0], yi = vs[i][1];
+        const xj = vs[j][0], yj = vs[j][1];
+        
+        const intersect = ((yi > y) !== (yj > y))
+                        && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    
+    return inside;
+}
